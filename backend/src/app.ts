307b2333c5fa096bb2ea14b1d8581bced298738a
Ghesker-afk <spacexz12345@gmail.com>
2026-01-08 14:26:30 -0300

    feat: create Express application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,2 +1,26 @@
 // this is where our Express app will run, and it will
 // be called on the 'server.ts' file.
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+export function createApp() {
+  const app = express();
+
+  // 
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+
+  app.use(express.json());
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  return app;
+}
