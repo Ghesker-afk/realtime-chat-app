6d43a97cc4a57d61a701addac0532770e0bf51f6
Ghesker-afk <spacexz12345@gmail.com>
2026-01-11 21:30:56 -0300

    feat: create the Update Route with the PATCH verb

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import {z} from "zod";
 import { UserProfile, UserProfileResponse } from "../modules/users/user.types.js";
+import { getAuth } from "@clerk/express";
+import { UnauthorizedError } from "../lib/errors.js";
 
 export const userRouter = Router();
 
@@ -35,3 +37,48 @@ function toResponse(profile: UserProfile): UserProfileResponse {
 
 
 // PATCH /api/me
+
+userRouter.patch("/", async (req, res, next) => {
+  try {
+    // first, we need to verify if the user is authenticated
+    // or not
+    const auth = getAuth(req);
+
+    if (!auth.userId) {
+      throw new UnauthorizedError("Unauthorized");
+    }
+
+    // we are going to do a basic body validation with
+    // the body response received from the UI side
+    const parsedBody = UserProfileUpdateSchema.parse(req.body);
+
+
+    const displayName = parsedBody.displayName && parsedBody.displayName.trim().length > 0 ? parsedBody.displayName.trim() : undefined;
+
+    const handle = parsedBody.handle && parsedBody.handle.trim().length > 0 ? parsedBody.handle.trim() : undefined;
+
+    const bio = parsedBody.bio && parsedBody.bio.trim().length > 0 ? parsedBody.bio.trim() : undefined;
+
+    const avatarUrl = parsedBody.avatarUrl && parsedBody.avatarUrl.trim().length > 0 ? parsedBody.avatarUrl.trim() : undefined;
+
+    try {
+      const profile = await updateUserProfile({
+        clerkUserId: auth.userId,
+        displayName,
+        handle,
+        bio,
+        avatarUrl
+      });
+
+      const response = toResponse(profile);
+
+      res.json({
+        data: response
+      });
+    } catch (err: any) {
+      throw err;
+    }
+  } catch (err: any) {
+    next(err);
+  }
+})
