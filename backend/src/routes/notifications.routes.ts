61b7631796670c8bda25143dc92fc0ec74a63a5f
Ghesker-afk <spacexz12345@gmail.com>
2026-01-23 11:54:03 -0300

    feat: create the replies migration

---
+++ backend/src/routes/notifications.routes.ts
@@ -0,0 +1,13 @@
+
+
+// GET the unread only notifications list
+// /api/notifications/unreadonly=true|false
+
+// POST to mark all the notifications as read
+// /api/notifications/read-all
+
+// POST to when we click in one specific notification, we
+// can go to there
+// /api/notifications/:id
+
+// we will come back at 7:42:05
