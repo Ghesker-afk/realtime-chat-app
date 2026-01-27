6b523725d1ee9642225b53032b62caeb260ef8ce
Ghesker-afk <spacexz12345@gmail.com>
2026-01-27 14:25:19 -0300

    feat: create the migration for the Direct Messages (DM)

--- frontend/src/components/threads/threads-home.tsx
+++ frontend/src/components/threads/threads-home.tsx
@@ -1,7 +1,21 @@
 "use client";
+import { createBrowserApiClient } from "@/lib/api-client";
+import { Category } from "@/types/thread";
+import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMemo, useState } from "react";
 
 function ThreadsHomePage() {
+  const { getToken } = useAuth();
+  const router = useRouter();
+
+  const apiClient = useMemo(() => createBrowserApiClient( getToken), [getToken]);
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // stop at 4:56:42
+
   return (
     <div>
       <Link href={"/threads/new"}>Add new thread</Link>
